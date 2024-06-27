72c89fdc1060be9b997d55d4b3192c133714e347
Ilias Martis <ilias@martis.dev>
2024-06-27 10:05:17 +0200

    Update run script. use run.sh -h for more info

--- bin/run.sh
+++ bin/run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+CLEAN_DOCKER() {
+  echo "Cleaning Docker containers, images, and volumes"
+  docker container prune --force
+  docker image prune --all --force
+  docker volume prune --force
+}
+
 if [[ $1 == "clean" || $1 == "clear" ]]; then
-  docker container prune --force && docker image prune --all --force && docker volume prune --force
-  exit 0
-elif [[ ($2 == "prod" || $2 == "dev" || $2 == "") ]] && [[ ($1 == "up" || $1 == "down") ]]; then
-
-  if [[ $2 != "prod" ]]; then
-    COMPOSE_PROFILES="dev"
-  else
-    COMPOSE_PROFILES=$2
-  fi
-
-  ACTION=$1
-
+  CLEAN_DOCKER
+elif [[ $1 == "down" ]]; then
+  docker compose down
+else
   HOMEDIR=$(basename "$PWD")
   if [[ $HOMEDIR == "bin" ]]; then
     cd ..
@@ -20,12 +19,51 @@ elif [[ ($2 == "prod" || $2 == "dev" || $2 == "") ]] && [[ ($1 == "up" || $1 == 
     cd ../..
   fi
 
+  DEMONIZE=""
+  CLEAN=false
+  COMPOSE_PROFILES="dev"
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+      echo "Usage: run.sh [OPTIONS]"
+      echo "Options:"
+      echo "  -h, --help          Show this help message and exit"
+      echo "  -d, --demonize      Run Docker in the background"
+      echo "  -c, --clean         Clean Docker containers, images, and volumes before running"
+      echo "  -p, --production    Run Docker in production mode"
+      echo "  -d, --development   Run Docker in development mode"
+      exit 0
+    ;;
+    -d | --demonize)
+      DEMONIZE="-d"
+      shift
+      ;;
+    -c | --clean)
+      CLEAN=true
+      shift
+      ;;
+    -p | --production | --prod)
+      COMPOSE_PROFILES="prod"
+      shift
+      ;;
+    -d | --development | --dev)
+      COMPOSE_PROFILES="dev"
+      shift
+      ;;
+    *)
+      break
+      shift
+      ;;
+    esac
+  done
+
   ENV_FILE="./config/.env.$COMPOSE_PROFILES"
   export ENV_FILE
 
-  echo "Running $ACTION for $COMPOSE_PROFILES with ENV file $ENV_FILE"
-  docker compose --env-file $ENV_FILE --profile $COMPOSE_PROFILES $ACTION
-else
-  echo "Invalid argument. Please use format ./run.sh [up|down|clean] [prod|dev]"
-  exit 1
+  if [[ $CLEAN == true ]]; then
+    CLEAN_DOCKER
+  fi
+
+  echo "Running Docker for $COMPOSE_PROFILES with ENV file $ENV_FILE"
+  docker compose --env-file $ENV_FILE --profile $COMPOSE_PROFILES up $DEMONIZE
 fi
