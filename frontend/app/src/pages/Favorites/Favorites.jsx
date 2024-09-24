b389d7454c55170d9baa95aabb52f7497023957e
mi444k <ilias@martis.dev>
2024-09-24 11:35:16 +0200

    Add Favorites

--- frontend/app/src/pages/Favorites/Favorites.jsx
+++ frontend/app/src/pages/Favorites/Favorites.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './Favorites.css'
+import React, { useEffect, useState } from "react";
+import "./Favorites.css";
+
+import { fertchUserFavarites } from "../../handlers/api";
+import { ProductCard } from "../../components/ProductCard";
 
 export default function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const favData = await fertchUserFavarites(null);
+      setFavorites(favData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className='Favorites'>Favorites
-        <h1>Favorites</h1>
+    <div className="Favorites">
+      <h1>Favorites</h1>
+      {favorites.length === 0 && <p>No favorites yet</p>}
+
+      <div classStyle="grip grip-template-colums">
+        {favorites?.map((favorite, index) => (
+          <ProductCard key={index} favorite={favorite} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
