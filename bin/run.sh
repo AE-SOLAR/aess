a595a1cbc3fdefa56f989445ae5e399bbd1d4015
mi444k <ilias@martis.dev>
2024-09-26 13:50:19 +0200

    Update runner

--- bin/run.sh
+++ bin/run.sh
@@ -8,6 +8,7 @@ CLEAN_DOCKER() {
   docker container prune --filter "label=prog=aeshop" --force
   docker image prune --filter "label=prog=aeshop" --force
   docker volume prune --filter "label=prog=aeshop" --force
+  docker buildx prune -f
 }
 
 if [[ $1 == "clean" || $1 == "clear" ]]; then
