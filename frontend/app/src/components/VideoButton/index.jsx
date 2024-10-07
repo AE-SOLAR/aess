7ea1a9282c83f269480f77c5b26e4bc882b4dda3
mi444k <ilias@martis.dev>
2024-10-07 10:45:16 +0200

    Add video in homepage buttons

---
+++ frontend/app/src/components/VideoButton/index.jsx
@@ -0,0 +1,33 @@
+import React, { useRef } from "react";
+
+const BackgroundVideo = ({ videoSrc, children }) => {
+  const videoRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    videoRef.current.play();
+  };
+
+  const handleMouseLeave = () => {
+    videoRef.current.pause();
+    videoRef.current.currentTime = 0;
+  };
+
+  return (
+    <div
+      className="relative w-full h-full"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <video
+        ref={videoRef}
+        src={videoSrc}
+        muted
+        loop={false}
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+      />
+      <div className="relative z-10">{children}</div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
