6051a8e229a3a0cf651a1d9391b2ed1a3c14ea04
mi444k <ilias@martis.dev>
2024-10-07 17:40:13 +0200

    Update Search input css

--- frontend/app/src/components/Input/SearchInput.jsx
+++ frontend/app/src/components/Input/SearchInput.jsx
@@ -24,11 +24,14 @@ const SearchInput = () => {
     <div
       className={`${style.searchContainer} ${isFocused ? style.active : ""}`}
     >
-      <FaSearchengin size={"1.5rem"} className={style.icon} />
+      <FaSearchengin
+        size={"1.5rem"}
+        className="absolute left-2 text-[1.5rem] transition-[left,opacity] duration-400 ease-in-out text-brand-dark-gray dark:text-[--color-brand-white-text] text-[--color-brand-dark-gray]"
+      />
       <input
         type="text"
         placeholder="Search"
-        className={style.input}
+        className={`${style.input} w-0 h-full pl-[2.5rem] bg-transparent outline-none border border-brand-light-gray rounded-[20px] transition-[width,opacity,padding] duration-400 ease-in-out opacity-0 text-[--color-brand-dark-gray] dark:text-[--color-brand-white-text]`}
         value={searchValue}
         onChange={handlerChange}
         onFocus={handleFocus}
