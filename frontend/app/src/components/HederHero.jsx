5e55fc4d372e7c102d4785d471b88734a6c03b6b
mi444k <ilias@martis.dev>
2024-10-09 08:55:54 +0200

    Update HeaderHero, added RedLine.

--- frontend/app/src/components/HederHero.jsx
+++ frontend/app/src/components/HederHero.jsx
@@ -1,22 +1,32 @@
 // 518 - 75 = 443
 
-const HeaderHero = ({ imageURL, children, onHeader = false }) => {
+import { RedLine } from "./ui/red-line";
+
+const HeaderHero = ({
+  imageURL,
+  children,
+  onHeader = false,
+  redLine = true,
+}) => {
   return (
-    <div
-      className={`relative z-10 ${
-        onHeader === true
-          ? "overflow-x-visible w-[calc(1440px - 1rem)]"
-          : "overflow-x-hidden w-[100%]"
-      } h-[443px] flex flex-col justify-center align-center text-white text-4xl font-bold `}
-    >
-      <img
-        src={imageURL}
-        alt="header_image"
-        className={`z-10 absolute object-cover inset-0 h-full w-auto min-w-full min-h-full ${
-          onHeader === true ? `h-[518px] top-[-75px] left-0` : `h-[443px]`
-        }`}
-      />
-      <div className="relative">{children}</div>
+    <div className={redLine && "pb-4"}>
+      <div
+        className={`relative z-10 ${
+          onHeader === true
+            ? "overflow-x-visible w-[calc(1440px - 1rem)]"
+            : "overflow-x-hidden w-[100%]"
+        } h-[443px] flex flex-col justify-center align-center text-white text-4xl font-bold `}
+      >
+        <img
+          src={imageURL}
+          alt="header_image"
+          className={`z-10 absolute object-cover inset-0 h-full w-auto min-w-full min-h-full ${
+            onHeader === true ? `h-[518px] top-[-75px] left-0` : `h-[443px]`
+          }`}
+        />
+        <div className="relative">{children}</div>
+      </div>
+      {redLine && <RedLine />}
     </div>
   );
 };
