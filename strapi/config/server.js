35b4679cb252d33018db2c68caffb4c2194e0952
mi444k <ilias@martis.dev>
2024-10-10 12:51:48 +0200

    Update Strapi URL

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,13 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('STRAPI_HOST', '0.0.0.0'),
-  port: env.int('STRAPI_PORT', 1337),
-  server: {
-    url: env('STRAPI_URL', 'https://devshop.ae-solar.com/strapi'),
-  },
+  host: env("STRAPI_HOST", "0.0.0.0"),
+  port: env.int("STRAPI_PORT", 1337),
+  url: env("STRAPI_URL", "https://devshop.ae-solar.com/strapi"),
   app: {
-    keys: env.array('STRAPI_APP_KEYS'),
+    keys: env.array("STRAPI_APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
