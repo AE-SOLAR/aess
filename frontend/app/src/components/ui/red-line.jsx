c7f87cc79f3c08ccc118947745b318510c9c010b
mi444k <ilias@martis.dev>
2024-10-10 10:50:33 +0200

    Update RedLine and add it to header and footer.

--- frontend/app/src/components/ui/red-line.jsx
+++ frontend/app/src/components/ui/red-line.jsx
@@ -6,6 +6,7 @@ export const RedLine = ({ width = "100%" }) => {
       style={{
         width: "100%",
         height: "2px",
+        opacity: "0.5",
         background:
           "linear-gradient(90deg, rgba(246, 1, 9, .1) 0%, rgba(246, 1, 9, .4) 25%, rgba(246, 1, 9, 1) 40%, rgba(246, 1, 9, 1) 60%, rgba(246, 1, 9, .4) 75%, rgba(246, 1, 9, .1) 100%)",
         boxShadow:
